e3c97b4497238cb2cde827d0602bfa1447172073
21hg3455 <119503376+21hg3455@users.noreply.github.com>
2023-01-10 09:53:49 +0000

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,81 +1,21 @@
-from sr.robot3 import *
-
-R = Robot()
-
-distance = R.ruggeduino.pins[A5].analogue_read()
-print(f"Rear ultrasound distance: {distance} meters")
-
-def Move(direction):
-    R.motor_board.motors[0].power = direction
-    R.motor_board.motors[1].power = direction
-
-#Reminder - make this intituive to the programs needs, eg. not hardcoded
-def RotateRobot():
-    R.motor_board.motors[0].power = 0.75
-    R.motor_board.motors[1].power = 0.25
-
-def AdjustBothClaws(adjustment):
-    AdjustClaw(0, adjustment)
-    AdjustClaw(1, adjustment)
-
-def AdjustClaw(claw, adjustment):
-    R.servo_board.servos[claw].position = adjustment
-
-def MoveAwayFromObject(pin):
-    if R.ruggeduino.pins[A5].analogue_read() < 2:
-        Move(1)
-
-def MoveTowardObject(pin):
-    if R.ruggeduino.pins[pin].analogue_read() < 2:
-        Move(1)
-
-def SpotMarkers():
-    return R.camera.see()
-
-def NoticeUserAboutMarkers(currentMarkers, homeMarkers):
-    print("I can view", len(currentMarkers), "markers:")
-
-    for m in currentMarkers:
-        print(" - Marker #{0} is {1} metres away with rotation x = {2}, z = {3}".format(m.id, m.distance / 1000, m.orientation.rot_x, m.orientation.rot_z))
-        CheckMarkerWithinOurArea(m, homeMarkers)
-        
-def CheckMarkerWithinOurArea(marker, homeMarkers):
-    if(marker.id <= homeMarkers[0]) or (marker.id >= homeMarkers[1]):
-      if(marker.id == 99):
-        print("This is a box")
-      else:
-        print("This is within our area")
-    else:
-        print("This is not within our area")
-
-def CollectWallMarkers(currentMarkers):
-    wallMarkers = []
-    for m in currentMarkers:
-        if(m != 99):
-            wallMarkers.append(m)
-    return wallMarkers
-
-def CheckMarkerCloseness(m):
-    if m.distance / 1000 < 2:
-        Move(1)
-
-
-RotateRobot()
-R.sleep(0.6)
-Move(0)
-Move(-1)
-R.sleep(1)
-Move(0)
-
-homeMarkers = [3, 24]
-NoticeUserAboutMarkers(SpotMarkers(), homeMarkers)
-MoveAboutTheBoard(homeMarkers)
-wallMarkers = CollectWallMarkers(SpotMarkers())
-NoticeUserAboutMarkers(wallMarkers, homeMarkers)
-
-R.sleep(0.50)
-MoveSpeed = 0.5
-
-#R.sleep(0.6/abs(MoveSpeed))
-#Reminder - move speed is how fast the robot moves. negative is backwards.
-#Reminder- set time divided by move speed allows for same distance
+def MoveToTarget(targetMarker):
+    Move(0.5)
+    Move(0.5)
+    distance = targetMarker.distance
+    while distance / 1000 > 0.2:
+        minRot = 0.02
+        markers = R.camera.see()
+        for m in markers:
+            zRot = m.orientation.rot_z
+            if m.id == 99 and abs(zRot) < abs(minRot) + 0.02:
+                print("valid marker at z = {}".format(zRot))
+                if zRot < 0 and R.motor_board.motors[0].power > -0.95:
+                    R.motor_board.motors[0].power += zRot
+                    R.motor_board.motors[1].power -= zRot
+                elif zRot > 0 and R.motor_board.motors[0].power < 0.95:
+                    R.motor_board.motors[0].power += zRot
+                    R.motor_board.motors[1].power -= zRot
+                distance = m.distance
+                minRot = zRot
+                R.sleep(0.01)
+                print(" - Target Marker is {0} metres away with rotation x = {1}, z = {2}".format(distance / 1000, minRot, zRot))
