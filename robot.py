d9e2a0e8d296386ec1565e9cf7ea64fea225ec0b
21hc3547 <119412202+21hc3547@users.noreply.github.com>
2023-02-13 22:30:06 +0000

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,13 +1,19 @@
-def Move(direction):
+def Move(direction): #moves the robot forwards and backwards
     R.motor_board.motors[0].power = direction
     R.motor_board.motors[1].power = direction
 
 
-def RotateRobot():
+def RotateRobotLeft(): #rotates robot left in an anti-clockwise direction
+    R.motor_board.motors[0].power = 0
+    R.motor_board.motors[1].power = 0.5
+    
+def RotateRobotRight(): #rotates robot right in a clockwise direction
+    R.motor_board.motors[0].power = 0.5
+    R.motor_board.motors[1].power = 0
+
+def RobotUTurn(): #roates robot 180 degrees
     R.motor_board.motors[0].power = 0
     R.motor_board.motors[1].power = 1
     R.sleep(0.53)
     Move(0)
 
-    #Code above rotates robot 180 degrees
-    #
