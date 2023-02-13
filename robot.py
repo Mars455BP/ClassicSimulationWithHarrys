bad73da1ce1aa8c06a897dc8b22edf55e4f62112
21hc3547 <119412202+21hc3547@users.noreply.github.com>
2023-02-13 22:20:18 +0000

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,1 +1,8 @@
+def RotateRobot():
+    R.motor_board.motors[0].power = 0 
+    R.motor_board.motors[1].power = 1 
+    R.sleep(0.53)
+    Move(0)
 
+    #Code above rotates robot 180 degrees
+    #
