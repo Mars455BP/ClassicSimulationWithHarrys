cc1fda0f78e8baab431303cab9df43c7b0b02fea
21hc3547 <119412202+21hc3547@users.noreply.github.com>
2023-02-24 11:19:02 +0000

    Update robot.py

--- robot.py
+++ robot.py
@@ -2,17 +2,24 @@ from sr.robot3 import *
 
 R = Robot()
 
-distance = R.ruggeduino.pins[A5].analogue_read()
-print(f"Rear ultrasound distance: {distance} meters")
-
-def Move(direction):
+def Move(direction): #moves the robot forwards and backwards
     R.motor_board.motors[0].power = direction
     R.motor_board.motors[1].power = direction
 
-#Reminder - make this intituive to the programs needs, eg. not hardcoded
-def RotateRobot():
-    R.motor_board.motors[0].power = 0.75
-    R.motor_board.motors[1].power = 0.25
+
+def RotateRobotLeft(): #rotates robot left in an anti-clockwise direction
+    R.motor_board.motors[0].power = 0
+    R.motor_board.motors[1].power = 0.5
+    
+def RotateRobotRight(): #rotates robot right in a clockwise direction
+    R.motor_board.motors[0].power = 0.5
+    R.motor_board.motors[1].power = 0
+
+def RobotUTurn(): #roates robot 180 degrees
+    R.motor_board.motors[0].power = 0
+    R.motor_board.motors[1].power = 1
+    R.sleep(0.53)
+    Move(0)
 
 def AdjustBothClaws(adjustment):
     AdjustClaw(0, adjustment)
@@ -21,58 +28,22 @@ def AdjustBothClaws(adjustment):
 def AdjustClaw(claw, adjustment):
     R.servo_board.servos[claw].position = adjustment
 
-def MoveAwayFromObject(pin):
-    if R.ruggeduino.pins[A5].analogue_read() < 2:
-        Move(1)
+def AdjustBothFingers(adjustment):
+    AdjustFinger(2, adjustment)
+    AdjustFinger(3, adjustment)
 
-def MoveTowardObject(pin):
-    if R.ruggeduino.pins[pin].analogue_read() < 2:
-        Move(1)
+def AdjustFinger(finger, adjustment):
+    R.servo_board.servos[finger].position = adjustment
 
-def SpotMarkers():
-    return R.camera.see()
 
-def NoticeUserAboutMarkers(currentMarkers, homeMarkers):
-    print("I can view", len(currentMarkers), "markers:")
-
-    for m in currentMarkers:
-        print(" - Marker #{0} is {1} metres away".format(m.id, m.distance / 1000))
-        CheckMarkerWithinOurArea(m, homeMarkers)
-        
-def CheckMarkerWithinOurArea(marker, homeMarkers):
-    if(marker.id <= homeMarkers[0]) or (marker.id >= homeMarkers[1]):
-        print("This is within our box")
-    else:
-        print("This is not within our area")
-
-def CollectWallMarkers(currentMarkers):
-    wallMarkers = []
-    for m in currentMarkers:
-        if(m != 99):
-            wallMarkers.append(m)
-    return wallMarkers
-
-def CheckMarkerCloseness(m):
-    if m.distance / 1000 < 2:
-        Move(1)
-
-RotateRobot()
-R.sleep(0.6)
+RotateRobotLeft()
+R.sleep(0.49)
 Move(0)
-Move(-1)
-R.sleep(1)
+Move(1)
+R.sleep(0.7)
 Move(0)
+AdjustBothClaws(1)
 
-homeMarkers = [3, 24]
-NoticeUserAboutMarkers(SpotMarkers(), homeMarkers)
-wallMarkers = CollectWallMarkers(SpotMarkers())
-NoticeUserAboutMarkers(wallMarkers, homeMarkers)
+#AdjustBothFingers(1)
 
 
-
-R.sleep(0.50)
-MoveSpeed=0.5
-
-#R.sleep(0.6/abs(MoveSpeed))
-#Reminder - move speed is how fast the robot moves. negative is backwards.
-#Reminder- set time divided by move speed allows for same distance
